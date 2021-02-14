6dc5e198594da4ce568c46809403c11fa923691d
jeyeonkim <jeyeonkim7@gmail.com>
2021-02-14 15:24:45 +0900

    0214 react

--- movie_app_2021/src/components/Movie.js
+++ movie_app_2021/src/components/Movie.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import './Movie.css';
+import { Link } from 'react-router-dom';
 // import propTypes from 'prop-types';
 
 function Movie({ title, year, summary, poster, genres }) {
@@ -10,13 +11,13 @@ function Movie({ title, year, summary, poster, genres }) {
             <div className="card-body">
                 <h3 className="movie__title">{ title }</h3>
                 <h5 className="movie__year">{ year }</h5>
-                <p className="movie__summary">{ summary }</p>
+                <p className="movie__summary">{ summary.slice(0, 180)}</p>
                 <ul className="movie__genres">
                     { genres.map((genre, index) => {
                         return <li key={ index } className="movie__genre">{ genre }</li>
                     })}
                 </ul>
-                <a href="#" className="btn btn-primary">Go somewhere</a>
+                <Link to={{ pathname: '/movie_detail', state: {year, title, summary, poster, genres, }}} className="btn btn-primary">Movie Detail</Link>
             </div>
         </div>
     );
